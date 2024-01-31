d4d1da0bd65d5fa58bb58d0efea75c2f2f59c7fc
librasvii <moreiralucas511@gmail.com>
2024-01-30 21:02:57 -0300

    update

--- chapter_one/characterCount/secondVersion/charCountInputC(for).c
+++ chapter_one/characterCount/secondVersion/charCountInputC(for).c
@@ -1,17 +1,16 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /*count characters in input;  2nd version*/
 
-int main(){
+int main() {
 
-    double nc;
+  double nc;
 
-    for(nc=0; getchar() != EOF; nc++)
+  for (nc = 0; getchar() != EOF; nc++)
+    ; // for statement require a body on C, but the semicolon isolated
+      //(called null statement) is there to satisfy that requeriment
 
-        ; //for statement require a body on C, but the semicolon isolated  
-          //(called null statement) is there to satisfy that requeriment
+  printf("%.0f\n", nc);
 
-    printf("%.0f\n", nc);
-
-    return 0;
+  return 0;
 }
