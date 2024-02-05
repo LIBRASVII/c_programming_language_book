135bcefbf07759b727f077d346423fd5cd5a5515
librasvii <moreiralucas511@gmail.com>
2024-02-05 20:57:37 -0300

    update

--- chapter_one/characterCount/firstVersion/charCountInputC(while).c
+++ chapter_one/characterCount/firstVersion/charCountInputC(while).c
@@ -1,15 +1,15 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /*count characters in input; 1st version*/
 
-int main(){
-    long nc;
+int main() {
+  long nc;
 
-    nc = 0;
-    while(getchar() != EOF){
-        ++nc;
+  nc = 0;
+  while (getchar() != EOF) {
+    ++nc;
     printf("%ld\n", nc);
-    }
+  }
 
-    return 0;
+  return 0;
 }
