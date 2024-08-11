e8355f75b7e564e46db3d0acbb84fb39404b0502
librasvii <moreiralucas511@gmail.com>
2024-08-11 12:15:06 -0300

    update

--- chapter_one/arrays_1.6/histogram.c
+++ chapter_one/arrays_1.6/histogram.c
@@ -7,13 +7,18 @@ int main() {
   char string[] = "C Programming Language";
   int array[] = {};
   int i = 0;
+  char c;
 
   for (int i = 0; i <= strlen(string); i++)
     if (string[i] != '\n')
       array[i] = atoi(string);
 
-  while (array[i])
-    printf("%d", array[i]);
+  while ((c = getchar()) != EOF)
+    if ('a' == '0' || 'z' == '0') {
+      if ('A' == '0' || 'Z' == '0') {
+        ++array[c - '0'];
+      }
+    }
 
   return 0;
 }
